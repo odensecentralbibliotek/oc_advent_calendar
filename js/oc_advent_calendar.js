2bd67ddde831c53a889937776a6d96fc16c002ec
Tobias F. Petersen <tobsf@hotmail.com>
2016-11-23 13:38:22 +0100

    many updates. - preb for competition - allow editors and admins to open all gates despite of dates - only run competitons for one day and after that prevent submissions - added phone number to contact form - hide data if gate cant be opened.

--- js/oc_advent_calendar.js
+++ js/oc_advent_calendar.js
@@ -4,13 +4,84 @@
  * and open the template in the editor.
  */
 jQuery(document).ready(function($){
-    $('.advent-calendar-can-open').click(function(){
-        $(this).find('.advent-calendar-background-image').toggleClass('advent-calendar-gate-open');
-        var modalbody = $(this).find('.advent-calendar-day-content-modal').html();
-         $('.advent-calendar-modal-wrapper').find('.advent-calendar-modal-body').html(modalbody);
-        setTimeout(function(){ 
+    competitions_participated = [];
+    var cookie_data = $.cookie('oc_advent');
+    if(cookie_data !== undefined && cookie_data != null)
+    {
+        competitions_participated = JSON.parse(cookie_data);
+    }
+    
+    $('.advent-calendar-can-open').click(advant_gate_open);
+    $('.advent-calendar-can-open-staff').unbind('click');
+    $('.advent-calendar-can-open-staff').click(advant_gate_open);
+    
+    $('.advendt-calendar-close-button').click(function(){
+         $('.advent-calendar-modal-wrapper').fadeOut(600,function(){
+            // $('.advent-calendar-modal-wrapper').toggleClass('advent-calendar-modal-wrapper-show');
+             $('.advent-calendar-gate-open').toggleClass('advent-calendar-gate-open');
+         });
+    });
+    $('.advent-calendar-modal-submit-contact-form').click(function(){
+        var arguments = $('.advent-calendar-modal-contact-form form').serialize();
+        /*
+         * Make ajax email send email request.
+         */
         
-       
+        $.ajax({
+            method: "GET",
+            url: "/oc/advent/ajax/response",
+            data: arguments
+          })
+            .done(function( msg ) {
+              if(msg === "1")
+              {
+                var current_day_index = $('.advent-calendar-modal-wrapper').find('#Dayindex').val();
+                competitions_participated.push(current_day_index);
+                $.cookie("oc_advent", JSON.stringify(competitions_participated), { expires : 90 });
+                $('.advent-calendar-modal-contact-form form').toggle();
+                $('.advent-calendar-modal-contact-form-msg').html(Drupal.t('Tak for dit svar'));
+                $('.advent-calendar-modal-contact-form-msg').toggle();
+              }
+              else
+              {
+                  $('.advent-calendar-modal-contact-form-msg').html(Drupal.t('Vi oplevede desværre en fejl , prøv igen.'));
+                  $('.advent-calendar-modal-contact-form-msg').toggle();
+              }
+
+         });
+    });
+    function advant_gate_open(){
+        $(this).find('.advent-calendar-background-image').toggleClass('advent-calendar-gate-open');
+        var has_competition = $(this).find('.advent-calendar-enable-contact-form').html()
+        var current_day_index = $(this).find('.advent-calendar-enable-contact-day-index').html();
+        
+        $('.advent-calendar-modal-contact-form form').trigger('reset');
+        if($('.advent-calendar-modal-contact-form-msg').css('display') !== 'none')
+        {
+            $('.advent-calendar-modal-contact-form-msg').css('display','none');
+        }
+        if($.inArray(current_day_index,competitions_participated) !== -1)
+        {
+            $('.advent-calendar-modal-contact-form-msg').html(Drupal.t('Tak for dit svar'));
+            $('.advent-calendar-modal-contact-form-msg').toggle();
+            if($('.advent-calendar-modal-contact-form form').css('display') !== 'none' )
+            {
+                $('.advent-calendar-modal-contact-form form').css('display','none');
+            }
+        }
+        if(!$('.advent-calendar-modal-contact-form form').is(':visible') && has_competition === "1" && $.inArray(current_day_index,competitions_participated) === -1)
+        {
+           $('.advent-calendar-modal-contact-form form').css('display','block');
+        
+        }
+        
+        var modalbody = $(this).find('.advent-calendar-day-content-modal').html();
+        $('.advent-calendar-modal-wrapper').find('.advent-calendar-modal-body-text').html(modalbody);
+        
+        $day_index =  $(this).find('.advent-calendar-enable-contact-day-index').html();
+        $('.advent-calendar-modal-wrapper').find('#Dayindex').val($day_index);
+        
+        setTimeout(function(){ 
         //$('.advent-calendar-modal-wrapper').toggleClass('advent-calendar-modal-wrapper-show');
         $('.advent-calendar-modal-wrapper').fadeIn('600');
         }, 700);
@@ -18,14 +89,6 @@ jQuery(document).ready(function($){
          * Trigger a Dialog
          */
         
-    });
-    $('.advendt-calendar-close-button').click(function(){
-         $('.advent-calendar-modal-wrapper').fadeOut(600,function(){
-            // $('.advent-calendar-modal-wrapper').toggleClass('advent-calendar-modal-wrapper-show');
-             $('.advent-calendar-gate-open').toggleClass('advent-calendar-gate-open');
-         });
-         
-         
-    })
+    }
     
 });
--- templates/views-view--oc-advent-calendar.tpl.php
+++ templates/views-view--oc-advent-calendar.tpl.php
@@ -1,7 +1,8 @@
 <?php
 $transdate = date('m-d-Y', time());
-$is_december  = true; //date('m') == 12 ? true : false;
-$cur_date = 24;// date('d');
+$is_december  = date('m') == 12 ? true : false;
+$cur_date =  date('d');
+global $user;
 ?>
 <?php if ($header): ?>
     <div class="oc-advent-view-header">
@@ -11,7 +12,7 @@ $cur_date = 24;// date('d');
 <?php endif; ?>
 <div class="advent-calendar-frame">
     <?php foreach($view->result as $index => $node) { ?>
-    <div class="advent-calendar-gate-wrapper <?php echo $index < $cur_date && $is_december ? 'advent-calendar-can-open' : ''  ?>">
+    <div class="advent-calendar-gate-wrapper <?php echo $index < $cur_date && $is_december ? 'advent-calendar-can-open' : ' '; echo oc_advent_calendar_user_has_role('redaktør') || oc_advent_calendar_user_has_role('administrator') ? ' advent-calendar-can-open-staff' : ' '?>">
         <div class="advent-calendar-background-image ">
             <?php if($node->file_managed_field_data_field_advent_image_uri == null){ ?>
                 <img class="advent-calendar-day-image" src="/sites/all/modules/custom/oc_advent_calendar/images/<?php echo $index+1 ?>.png" />
@@ -19,8 +20,14 @@ $cur_date = 24;// date('d');
                 <img class="advent-calendar-day-image" src="<?php echo file_create_url($node->file_managed_field_data_field_advent_image_uri) ?>" />
             <?php }?>
             <span class="advent-calendar-day-text"><?php echo $node->field_data_field_position_field_position_value  ?></span>
-            <?php if($index < $cur_date && $is_december) { ?>
+            <?php if(($index < $cur_date && $is_december) || oc_advent_calendar_user_has_role('redaktør') || oc_advent_calendar_user_has_role('administrator')) { ?>
                 <div class="advent-calendar-day-content-modal"><?php echo isset($node->field_body[0]) ? $node->field_body[0]['rendered']['#markup'] : '' ?></div>
+                <?php if($cur_date == $index+1){ ?>
+                <div class="advent-calendar-enable-contact-form"><?php echo isset($node->field_field_konkurrence[0]) ?  $node->field_field_konkurrence[0]['raw']['value'] : 0 ?></div>
+                <?php }else{ ?>
+                <div class="advent-calendar-enable-contact-form">0</div>
+                <?php } ?>
+                <div class="advent-calendar-enable-contact-day-index"><?php echo $index+1 ?></div>
             <?php }?>
         </div>
      </div>
@@ -36,7 +43,21 @@ $cur_date = 24;// date('d');
         <div class="advent-calendar-modal-header">
             
         </div>
-        <div class="advent-calendar-modal-body">aaaa</div>
+        <div class="advent-calendar-modal-body">
+            <div class="advent-calendar-modal-body-text" style="height: 41%;"></div>
+            <div class="advent-calendar-modal-contact-form" >
+                <form style="display:none">
+                    <input name="Name" placeholder="Dit Fulde navn" type="text">
+                    <input name="Email" placeholder="Email" type="text">
+                    <input name="Tlf" placeholder="Telefon nummer" type="text">
+                    <textarea name="Text" placeholder="Skriv dit svar her" rows="5" resizable="true"></textarea>
+                    <input type="hidden" name="Dayindex" id="Dayindex" type="text">
+                    <a class="btn advent-calendar-modal-submit-contact-form">Indsend!<a/>
+                </form>
+            </div>
+            <div class="advent-calendar-modal-contact-form-msg" style="display:none"></div>
+        </div>
+
         <div class="advendt-calendar-close-button">
             <img title="Luk" src="/sites/all/modules/custom/oc_advent_calendar/images/close-icon.png">
 	</div>
